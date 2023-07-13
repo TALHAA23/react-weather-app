b98accafc5c96129c32bfbf75a6a33487151c444
Talha <talhasifat23@gamil.com>
2023-07-13 17:10:06 +0500

    setting API

---
+++ src/api/current-and-hourly.js
@@ -0,0 +1,36 @@
+export default async function getForcast() {
+    const forcast = await fetch(
+        "http://api.weatherapi.com/v1/forecast.json?key=013d87f7b7f644169ee154512231207&q=peshawar&days=1&aqi=no&alerts=no"
+    ).then((res) => res.json());
+
+    const location = forcast.location.name;
+    const currentWeather = forcast.current;
+    const sunrisesunset = forcast.forecast.forecastday[0].astro;
+    const hourlyForcast = forcast.forecast.forecastday[0].hour;
+
+    const {
+        wind_mph,
+        pressure_mb,
+        uv
+    } = await currentWeather;
+    const {
+        sunrise,
+        sunset
+    } = await sunrisesunset;
+
+    const weatherVariables = {
+        wind: `${wind_mph}Km/h`,
+        pressue: `${pressure_mb} hpa`,
+        uv: uv,
+        sunrise: sunrise,
+        sunset: sunset,
+    };
+
+    return {
+        location,
+        currentWeather,
+        sunrisesunset,
+        weatherVariables,
+        hourlyForcast,
+    };
+}
